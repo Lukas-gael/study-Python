93fa6434b0d2fdeb00b550424a6850fa0126eff1
Lucas Gael <Lukasgael.live@gmail.com>
2024-12-11 23:55:38 -0300

    4 study

---
+++ del and len.py
@@ -0,0 +1,13 @@
+hat_list = [1, 2, 3, 4, 5]  # This is an existing list of numbers hidden in the hat.
+
+
+# Step 1: write a line of code that prompts the user
+hat_list[2] = int(input())
+# to replace the middle number with an integer number entered by the user.
+del hat_list[-1]
+# Step 2: write a line of code that removes the last element from the list.
+print("list lenght: ", len(hat_list))
+# Step 3: write a line of code that prints the length of the existing list.
+
+print(hat_list)
+
